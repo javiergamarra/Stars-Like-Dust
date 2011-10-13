3b0cfcbc95bfbf78a03ce0e33c108f53e2e4d0d0
Javier Gamarra <nhpatt@gmail.com>
2011-10-13 13:03:11 +0200

    JSLint

--- js/game.js
+++ js/game.js
@@ -1,100 +1,101 @@
-var starsLikeDust = function() {
-	var canvas = document.getElementById("canvas");
+var starsLikeDust = (function () {
+	var CANVAS_HEIGHT = 500, canvas = document.getElementById("canvas");
 	canvas.width = window.innerWidth - 100;
-	canvas.height = 500;
+	canvas.height = CANVAS_HEIGHT;
+	var keys = [];
 	var ctx = canvas.getContext("2d");
 	var loop;
-	var keys = new Array();
-	var background = new Background();
-	var hero = new Hero(canvas.width, canvas.height);
-	window.addEventListener('keydown', doKeyDown, true);
-	window.addEventListener('keyup', doKeyUp, true);
-
-	var clear = function() {
-		canvas.width = canvas.width;
-	}
-
-	function Background() {
-		this.image = new Image();
-		this.image.src = "images/galaxy.jpg";
-	}
-
-	Background.prototype.draw = function(ctx) {
-		ctx.drawImage(this.image, 0, 0);
-	}
-
-	function Hero(width, height) {
-		this.image = new Image();
-		this.image.src = "images/hero.png";
-		this.canvasWidth = width;
-		this.canvasHeight = height;
-		this.x = 50;
-		this.y = height / 2;
-		this.setPosition = function(positionX, positionY) {
-			this.x = positionX;
-			this.y = positionY;
-		};
-		this.draw = function(ctx) {
-			ctx.drawImage(this.image, this.x, this.y);
-		};
-		this.moveLeft = function() {
-			if (this.x > 0) {
-				this.setPosition(this.x - 8, this.y);
-			}
-		};
-	}
-
-	Hero.prototype.moveRight = function() {
-		if (this.x + this.image.width < this.canvasWidth) {
-			this.setPosition(this.x + 8, this.y);
-		}
-	}
-
-	Hero.prototype.moveUp = function() {
-		if (this.y > 0) {
-			this.setPosition(this.x, this.y - 8);
-		}
-	}
-
-	Hero.prototype.moveDown = function() {
-		if (this.y + this.image.height < this.canvasHeight) {
-			this.setPosition(this.x, this.y + 9);
-		}
-	}
 
 	function doKeyDown(evt) {
 		keys[evt.keyCode] = true;
 	}
+	
 	function doKeyUp(evt) {
 		keys[evt.keyCode] = false;
 	}
+	
+	window.addEventListener('keydown', doKeyDown, true);
+	window.addEventListener('keyup', doKeyUp, true);
+	
+	var background = (function () {
+		var image = new Image();
+		image.src = "images/galaxy.jpg";
+		var draw = function (ctx) {
+			ctx.drawImage(image, 0, 0);
+		};
+		return {
+			draw : draw
+		};
+	}());
+
+	var hero = (function () {
+		var SPEED = 8;
+		var image = new Image();
+		image.src = "images/hero.png";
+		var x = 50;
+		var y = CANVAS_HEIGHT / 2;
+		var setPosition = function (positionX, positionY) {
+			x = positionX;
+			y = positionY;
+		};
+		var draw = function (ctx) {
+			ctx.drawImage(image, x, y);
+		};
+		var moveLeft = function () {
+			if (x > 0) {
+				setPosition(x - SPEED, y);
+			}
+		};
+		var moveRight = function () {
+			if (x + image.width < canvas.width) {
+				setPosition(x + SPEED, y);
+			}
+		};
+		var moveUp = function () {
+			if (y > 0) {
+				setPosition(x, y - 8);
+			}
+		};
+		var moveDown = function () {
+			if (y + image.height < canvas.height) {
+				setPosition(x, y + SPEED);
+			}
+		};
+		return {
+			draw : draw,
+			moveUp : moveUp,
+			moveDown : moveDown,
+			moveLeft : moveLeft,
+			moveRight : moveRight
+		};
+	}());
 
 	function move() {
-		if (38 in keys && keys[38] || 87 in keys && keys[87]) { // up
+		if ((keys[38] !== undefined && keys[38]) || (keys[87] !== undefined && keys[87])) { // up
 			hero.moveUp();
 		}
-		if (40 in keys && keys[40] || 83 in keys && keys[83]) { // down
+		if ((keys[40] !== undefined && keys[40]) || (keys[83] !== undefined && keys[83])) { // down
 			hero.moveDown();
 		}
-		if (37 in keys && keys[37] || 65 in keys && keys[65]) { // left
+		if ((keys[37] !== undefined && keys[37]) || (keys[65] !== undefined && keys[65])) { // left
 			hero.moveLeft();
 		}
-		if (39 in keys && keys[39] || 68 in keys && keys[68]) { // right
+		if ((keys[39] !== undefined && keys[39]) || (keys[68] !== undefined && keys[68])) { // right
 			hero.moveRight();
 		}
 	}
 
-	var start = function() {
+	var start = function () {
 		move();
-		clear();
+		canvas.width = canvas.width;
 		background.draw(ctx);
 		hero.draw(ctx);
 		loop = setTimeout(start, 1000 / 100);
-	}
+	};
 
 	return {
-		start : start,
-	}
+		start : start
+	};
 
-}();
+}());
 starsLikeDust.start();
